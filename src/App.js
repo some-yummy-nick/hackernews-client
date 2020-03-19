9fa06836ed62915af364b7098b6329b43c0043c3
Marat <yeffasol@mail.ru>
2020-03-19 21:58:57 +0300

    Add first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { PureComponent } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+	{
+		title: 'React',
+		url: 'https://reactjs.org/',
+		author: 'Jordan Walke',
+		num_comments: 3,
+		points: 4,
+		objectID: 0,
+	},
+	{
+		title: 'Redux',
+		url: 'https://redux.js.org/',
+		author: 'Dan Abramov, Andrew Clark',
+		num_comments: 2,
+		points: 5,
+		objectID: 1,
+	},
+];
+
+class App extends PureComponent {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			list,
+		};
+
+	}
+
+	onDismiss = id => {
+		const updatedList = this.state.list.filter(item => item.objectID !== id);
+		this.setState({ list: updatedList });
+	};
+
+	render() {
+		return (
+			<div className="App">
+				{this.state.list.map(item =>
+					<div key={`list ${item.objectID}`}>
+						<span>
+						<a href={item.url}>{item.title}</a>
+						</span>
+						<span> {item.author}</span>
+						<span> {item.num_comments}</span>
+						<span> {item.points}</span>
+						<span>
+							<button
+								onClick={() => this.onDismiss(item.objectID)}
+								type="button"
+							>
+							Отбросить
+							</button>
+						</span>
+					</div>
+				)}
+			</div>
+		);
+	}
 }
 
 export default App;
