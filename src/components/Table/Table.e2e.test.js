b0e2e1cd46dee725aaf9bfcda61bf1b23127f670
Marat <yeffasol@mail.ru>
2020-03-22 15:55:10 +0300

    Add end to end tests

---
+++ src/components/Table/Table.e2e.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {Table} from "./Table";
+
+Enzyme.configure({adapter: new Adapter()});
+
+const props = {
+	list: [
+		{ title: 'title 1', author: 'name 1', num_comments: 1, points: 2, objectID: 'y' },
+		{ title: 'title 2', author: 'name 2', num_comments: 1, points: 2, objectID: 'z' },
+	],
+};
+
+test('shows two items in list', () => {
+	const element = shallow(
+		<Table { ...props } />
+	);
+	expect(element.find('.table-row').length).toBe(2);
+});
